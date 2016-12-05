b47867897860e056619f6f2ca7c245a3eb4c95e3
Hadi <hadilashkari@gmail.com>
2016-12-05 12:47:09 +0330

    String corrections

--- persian/src/test/java/ir/hadilq/test/PersianCalendarUnitTest.java
+++ persian/src/test/java/ir/hadilq/test/PersianCalendarUnitTest.java
@@ -120,8 +120,8 @@ public class PersianCalendarUnitTest {
         Assert.assertTrue("Month: " + calendar.get(Calendar.MONTH), calendar.get(Calendar.MONTH) == 7);
         Assert.assertTrue("Day of month: " + calendar.get(Calendar.DAY_OF_MONTH), calendar.get(Calendar.DAY_OF_MONTH) == 23);
         Assert.assertTrue("Hour: " + calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.HOUR_OF_DAY) == 10);
-        Assert.assertTrue("Hour: " + calendar.get(Calendar.MINUTE), calendar.get(Calendar.MINUTE) == 56);
-        Assert.assertTrue("Hour: " + calendar.get(Calendar.SECOND), calendar.get(Calendar.SECOND) == 52);
+        Assert.assertTrue("Minute: " + calendar.get(Calendar.MINUTE), calendar.get(Calendar.MINUTE) == 56);
+        Assert.assertTrue("Second: " + calendar.get(Calendar.SECOND), calendar.get(Calendar.SECOND) == 52);
         Assert.assertTrue("Millis: " + calendar.get(Calendar.MILLISECOND), calendar.get(Calendar.MILLISECOND) == 901);
     }
 
